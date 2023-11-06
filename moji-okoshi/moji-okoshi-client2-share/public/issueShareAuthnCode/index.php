3b76f36c395cfc92530971c7de4c1cd24058baf8
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2023-11-07 05:56:01 +0900

    Regist DB

--- moji-okoshi/moji-okoshi-client2-share/public/issueShareAuthnCode/index.php
+++ moji-okoshi/moji-okoshi-client2-share/public/issueShareAuthnCode/index.php
@@ -50,7 +50,28 @@ if ( mb_strtolower($_SERVER['REQUEST_METHOD']) == 'post' ) {
 	$curl_res['timestamp'] = microtime(true);
 	$curl_res['sharecode'] = issue_num_rand(6);
 	$curl_res['authncode'] = hash('crc32', $curl_res['timestamp']);
+	$curl_res['connected'] = false;
 	
+	$pdo = new \PDO(
+		'pgsql:dbname=webapp host=database_mojiokoshi_v2 port=5432',
+		'postgres',
+		'passw0rd',
+		[
+			\PDO::ATTR_PERSISTENT => true,
+			\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+			\PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+		]
+	);
+	$stm = $pdo->prepare('INSERT INTO transcription (uuid, iat, sharecode, authncode, clientip, transcription) VALUES (?,?,?,?,?,?);');
+	$res = $stm->execute([
+		$curl_res['sharecode'] . $curl_res['timestamp'],
+		$curl_res['timestamp'],
+		$curl_res['sharecode'],
+		$curl_res['authncode'],
+		$_SERVER['REMOTE_ADDR'],
+		'***'
+	]);
+	$curl_res['connected'] = $res;
 
 	echo json_encode($curl_res);
 	exit();
