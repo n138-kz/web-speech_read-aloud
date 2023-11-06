d09b6776e1402d60e3901c4b3446bf63bece4b57
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2023-11-07 05:17:51 +0900

    Create public\issueShareAuthnCode\index.php

---
+++ moji-okoshi/moji-okoshi-client2-share/public/issueShareAuthnCode/index.php
@@ -0,0 +1,38 @@
+<?php session_start();
+
+$_SESSION = [];
+$curl_res = [];
+
+if (mb_strtolower($_SERVER['REQUEST_METHOD']) != 'get' ) {
+	http_response_code(405);
+	$curl_res['timestamp'] = time();
+	$curl_res['mesg']      = 'Method Not Allowed.';
+
+	echo json_encode($curl_res);
+	exit();
+}
+if ( !isset($_REQUEST) || !is_array($_REQUEST) ) {
+	http_response_code(400);
+	$curl_res['timestamp'] = time();
+	$curl_res['mesg']      = 'Bad Request.';
+
+	echo json_encode($curl_res);
+	exit();
+}
+if ( !isset($_REQUEST['timestamp']) || (! $_REQUEST['timestamp'] > 0) ) {
+	http_response_code(400);
+	$curl_res['timestamp'] = time();
+	$curl_res['mesg']      = 'Bad Request.';
+
+	echo json_encode($curl_res);
+	exit();
+}
+if ( abs(time()-$_REQUEST['timestamp'])>600 ) {
+	http_response_code(408);
+	$curl_res['timestamp'] = time();
+	$curl_res['mesg']      = 'Request Timeout.';
+
+	echo json_encode($curl_res);
+	exit();
+}
+
