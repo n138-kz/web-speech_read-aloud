ed2cd87232987bafb3310f97c0b41a759ae55476
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2023-11-07 04:20:50 +0900

    drop/create table transcription

--- moji-okoshi/moji-okoshi-client2-share/sql_create-table/transcription.sql
+++ moji-okoshi/moji-okoshi-client2-share/sql_create-table/transcription.sql
@@ -1,1 +1,10 @@
-﻿
+﻿drop table transcription
+create table transcription (
+   uuid          text  not null unique,
+   iat           float not null unique,
+   sharecode     text  not null,
+   authncode     text  not null,
+   clientip      text  not null,
+   transcription text  not null,
+   primary key ( uuid )
+)
